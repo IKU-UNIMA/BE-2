7934c4b03501e9dbabf1be852c32a74b459254bf
Adhicitta Masran <adhicittamasran@gmail.com>
2023-03-25 18:07:01 +0800

    add fakultas request and response structs

---
+++ src/api/response/fakultas.go
@@ -0,0 +1,23 @@
+package response
+
+type Fakultas struct {
+	ID   int    `json:"id"`
+	Nama string `json:"nama"`
+}
+
+type DetailFakultas struct {
+	ID    int              `json:"id"`
+	Nama  string           `json:"nama"`
+	Prodi []ProdiReference `gorm:"foreignKey:IdFakultas" json:"prodi"`
+}
+
+type ProdiReference struct {
+	ID         int    `json:"id"`
+	IdFakultas int    `json:"-"`
+	Nama       string `json:"nama"`
+	Jenjang    string `json:"jenjang"`
+}
+
+func (ProdiReference) TableName() string {
+	return "prodi"
+}
