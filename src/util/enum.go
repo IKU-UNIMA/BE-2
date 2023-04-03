c7badf4edee496b6167804fb0ab91bc3ea5c6481
Adhicitta Masran <adhicittamasran@gmail.com>
2023-04-03 22:48:37 +0800

    add user role enum

---
+++ src/util/enum.go
@@ -0,0 +1,10 @@
+package util
+
+type UserRole string
+
+const (
+	ADMIN     UserRole = "admin"
+	DOSEN     UserRole = "operator"
+	REKTOR    UserRole = "rektor"
+	MAHASISWA UserRole = "mahasiswa"
+)
