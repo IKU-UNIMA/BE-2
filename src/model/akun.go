34d677b551982a19b7690b115e91f3265d8fa706
Adhicitta Masran <adhicittamasran@gmail.com>
2023-04-05 14:54:07 +0800

    add dosen role enum in akun model

--- src/model/akun.go
+++ src/model/akun.go
@@ -4,7 +4,7 @@ type Akun struct {
 	ID        int       `gorm:"primaryKey"`
 	Email     string    `gorm:"type:varchar(255);unique"`
 	Password  string    `gorm:"type:varchar(255)"`
-	Role      string    `gorm:"type:enum('rektor', 'admin', 'operator', 'mahasiswa')"`
+	Role      string    `gorm:"type:enum('rektor', 'admin', 'operator', 'dosen', 'mahasiswa')"`
 	Admin     Admin     `gorm:"foreignKey:ID;constraint:OnDelete:CASCADE"`
 	Operator  Operator  `gorm:"foreignKey:ID;constraint:OnDelete:CASCADE"`
 	Rektor    Rektor    `gorm:"foreignKey:ID;constraint:OnDelete:CASCADE"`
