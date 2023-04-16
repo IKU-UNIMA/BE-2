0f7532f172babb0c9d0c8104d921d16600634dff
Adhicitta Masran <adhicittamasran@gmail.com>
2023-04-16 15:26:18 +0800

    move mahasiswa reference struct to mahasiswa.go

--- src/api/response/prestasi.go
+++ src/api/response/prestasi.go
@@ -13,31 +13,19 @@ type (
 	}
 
 	DetailPrestasi struct {
-		ID                int                `json:"id"`
-		IdMahasiswa       int                `json:"-"`
-		IdFakultas        int                `json:"-"`
-		IdProdi           int                `json:"-"`
-		IdSemester        int                `json:"-"`
-		IdDosenPembimbing int                `json:"-"`
-		Nama              string             `json:"nama"`
-		TingkatPrestasi   string             `json:"tingkat_prestasi"`
-		Penyelenggara     string             `json:"penyelenggara"`
-		Peringkat         string             `json:"peringkat"`
-		Sertifikat        string             `json:"sertifikat"`
-		Mahasiswa         MahasiswaReference `gorm:"foreignKey:IdMahasiswa" json:"mahasiswa"`
-		Fakultas          Fakultas           `gorm:"foreignKey:IdFakultas" json:"fakultas"`
-		Prodi             ProdiReference     `gorm:"foreignKey:IdProdi" json:"prodi"`
-		Semester          Semester           `gorm:"foreignKey:IdSemester" json:"semester"`
-		DosenPembimbing   Dosen              `gorm:"foreignKey:IdDosenPembimbing" json:"dosen_pembimbing"`
-	}
-
-	MahasiswaReference struct {
-		ID   int    `json:"id"`
-		Nama string `json:"nama"`
-		Nim  string `json:"nim"`
+		ID                int       `json:"id"`
+		IdMahasiswa       int       `json:"-"`
+		IdFakultas        int       `json:"-"`
+		IdProdi           int       `json:"-"`
+		IdSemester        int       `json:"-"`
+		IdDosenPembimbing int       `json:"-"`
+		Nama              string    `json:"nama"`
+		TingkatPrestasi   string    `json:"tingkat_prestasi"`
+		Penyelenggara     string    `json:"penyelenggara"`
+		Peringkat         string    `json:"peringkat"`
+		Sertifikat        string    `json:"sertifikat"`
+		Mahasiswa         Mahasiswa `gorm:"foreignKey:IdMahasiswa" json:"mahasiswa"`
+		Semester          Semester  `gorm:"foreignKey:IdSemester" json:"semester"`
+		DosenPembimbing   Dosen     `gorm:"foreignKey:IdDosenPembimbing" json:"dosen_pembimbing"`
 	}
 )
-
-func (MahasiswaReference) TableName() string {
-	return "mahasiswa"
-}
