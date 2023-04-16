d1ada09b88ac8248b1a782db91194cf6e81015fb
Adhicitta Masran <adhicittamasran@gmail.com>
2023-04-16 14:57:18 +0800

    add status field to the mahasiswa model

--- src/model/mahasiswa.go
+++ src/model/mahasiswa.go
@@ -5,6 +5,7 @@ type Mahasiswa struct {
 	IdProdi      int
 	Nama         string `gorm:"type:varchar(255)"`
 	Nim          string `gorm:"type:varchar(255);unique"`
+	Status       bool
 	JenisKelamin string `gorm:"type:varchar(1)"`
 	Prodi        Prodi  `gorm:"foreignKey:IdProdi;constraint:OnDelete:CASCADE"`
 }
