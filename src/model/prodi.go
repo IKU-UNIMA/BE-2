3b05f99f0ed3079e5bf2fd56ae9150e5b98eb0f1
Adhicitta Masran <adhicittamasran@gmail.com>
2023-04-16 14:59:41 +0800

    add relation with fakultas to the prodi model

--- src/model/prodi.go
+++ src/model/prodi.go
@@ -3,6 +3,7 @@ package model
 type Prodi struct {
 	ID         int `gorm:"primaryKey"`
 	IdFakultas int
-	Nama       string `gorm:"type:varchar(255);unique"`
-	Jenjang    string `gorm:"type:varchar(60)"`
+	Nama       string   `gorm:"type:varchar(255);unique"`
+	Jenjang    string   `gorm:"type:varchar(60)"`
+	Fakultas   Fakultas `gorm:"foreignKey:IdFakultas;constraint:OnDelete:CASCADE"`
 }
