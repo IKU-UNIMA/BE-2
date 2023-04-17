c13e40e8a44e0dd36e0225a1bf35623ce1ddba41
Adhicitta Masran <adhicittamasran@gmail.com>
2023-04-17 10:16:44 +0800

    add admin IKU 2 and admin IKU 2 with operator and mahasiswa authorization middleware

--- src/api/middleware/authorization.go
+++ src/api/middleware/authorization.go
@@ -11,7 +11,19 @@ func GrantAdminUmum(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		claims := util.GetClaimsFromContext(c)
 		if claims["role"].(string) != string(util.ADMIN) ||
-			claims["bagian"].(string) != "UMUM" {
+			claims["bagian"].(string) != util.UMUM {
+			return util.FailedResponse(c, http.StatusUnauthorized, nil)
+		}
+
+		return next(c)
+	}
+}
+
+func GrantAdminIKU2(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		claims := util.GetClaimsFromContext(c)
+		if claims["role"].(string) != string(util.ADMIN) ||
+			claims["bagian"].(string) != util.IKU2 {
 			return util.FailedResponse(c, http.StatusUnauthorized, nil)
 		}
 
@@ -23,6 +35,24 @@ func GrantMahasiswa(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		claims := util.GetClaimsFromContext(c)
 		if claims["role"].(string) != string(util.MAHASISWA) {
+			return util.FailedResponse(c, http.StatusUnauthorized, nil)
+		}
+
+		return next(c)
+	}
+}
+
+func GrantAdminIKU2OperatorAndMahasiswa(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		claims := util.GetClaimsFromContext(c)
+		role := claims["role"].(string)
+		bagian := claims["bagian"].(string)
+		if role != string(util.MAHASISWA) && role != string(util.ADMIN) &&
+			role != string(util.OPERATOR) {
+			return util.FailedResponse(c, http.StatusUnauthorized, nil)
+		}
+
+		if role == string(util.ADMIN) && bagian != util.IKU2 {
 			return util.FailedResponse(c, http.StatusUnauthorized, nil)
 		}
 
