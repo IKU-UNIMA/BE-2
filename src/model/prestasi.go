7576a1945ff2fdf90310e035101bdcca71de17df
Adhicitta Masran <adhicittamasran@gmail.com>
2023-04-18 11:52:06 +0800

    prestasi: add created at field to the model

--- src/model/prestasi.go
+++ src/model/prestasi.go
@@ -1,15 +1,18 @@
 package model
 
+import "time"
+
 type Prestasi struct {
 	ID                int `gorm:"primaryKey"`
 	IdMahasiswa       int
 	IdSemester        int
-	IdDosenPembimbing int       `gorm:"default:null"`
-	Nama              string    `gorm:"type:varchar(255)"`
-	TingkatPrestasi   string    `gorm:"type:varchar(60)"`
-	Penyelenggara     string    `gorm:"type:text"`
-	Peringkat         string    `gorm:"type:varchar(30)"`
-	Sertifikat        string    `gorm:"type:text"`
+	IdDosenPembimbing int    `gorm:"default:null"`
+	Nama              string `gorm:"type:varchar(255)"`
+	TingkatPrestasi   string `gorm:"type:varchar(60)"`
+	Penyelenggara     string `gorm:"type:text"`
+	Peringkat         string `gorm:"type:varchar(30)"`
+	Sertifikat        string `gorm:"type:text"`
+	CreatedAt         time.Time
 	Mahasiswa         Mahasiswa `gorm:"foreignKey:IdMahasiswa;constraint:OnDelete:CASCADE"`
 	Semester          Semester  `gorm:"foreignKey:IdSemester;constraint:OnDelete:CASCADE"`
 	DosenPembimbing   Dosen     `gorm:"foreignKey:IdDosenPembimbing;constraint:OnDelete:SET NULL"`
