e80a5249104031c6d13e32eff584412a37fe9bc1
Adhicitta Masran <adhicittamasran@gmail.com>
2023-04-19 15:04:46 +0800

    validation: get the json tag name as the error key instead of transform the field name

--- src/util/validation/validation.go
+++ src/util/validation/validation.go
@@ -3,7 +3,7 @@ package validation
 import (
 	"be-2/src/util"
 	"net/http"
-	"regexp"
+	"reflect"
 	"strings"
 
 	"github.com/go-playground/validator/v10"
@@ -15,6 +15,15 @@ type CustomValidator struct {
 }
 
 func (cv *CustomValidator) Validate(i interface{}) error {
+	cv.Validator.RegisterTagNameFunc(func(fld reflect.StructField) string {
+		name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
+		if name == "-" {
+			return ""
+		}
+
+		return name
+	})
+
 	if err := cv.Validator.Struct(i); err != nil {
 		errs := err.(validator.ValidationErrors)
 		httpCode := http.StatusBadRequest
@@ -31,17 +40,10 @@ func (cv *CustomValidator) Validate(i interface{}) error {
 func translate(errs validator.ValidationErrors) map[string]string {
 	errors := map[string]string{}
 	for _, e := range errs {
-		key := createErrorKey(e.Field())
-		errors[key] = getTagMessage(e)
+		errors[e.Field()] = getTagMessage(e)
 	}
 
 	return errors
-}
-
-func createErrorKey(key string) string {
-	var matchAllCap = regexp.MustCompile("([a-z0-9])([A-Z])")
-	snake := matchAllCap.ReplaceAllString(key, "${1}_${2}")
-	return strings.ToLower(snake)
 }
 
 func getTagMessage(e validator.FieldError) string {
